aa18667abef8f55d48ef5f1acf9ca877e62f4c77
Juan A. de la Puente <jpuente@dit.upm.es>
2020-04-03 12:53:42 +0200

    refactor temperature conversionutility

--- utilities/src/temperature.adb
+++ utilities/src/temperature.adb
@@ -7,13 +7,15 @@ with TTC_Data.Strings;    use TTC_Data.Strings;
 with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
-procedure Main is
+procedure Temperature is
    Raw_Value : Integer;
 begin
+   Put_line("Convert raw values from a temperature sensor to Celsius");
+   Put_Line("Enter an integer value, or 0 to exit");
    loop
       Put("> ");
       Get(Raw_Value);
       exit when Raw_Value <= 0;
       Put_Line(Image(Temperature(Sensor_Reading(Raw_Value))));
    end loop;
-end Main;
+end Temperature;
